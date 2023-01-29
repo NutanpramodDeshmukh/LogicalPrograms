bc28ff521dc2e6a74bab898b5aab4dec3bdfbda9
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-01-30 00:20:10 +0530

    Added: files

---
+++ Stopwatch/Stopwatch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+           
+            stopwatch.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("HI");
+            }
+            
+            stopwatch.Stop();
+            Console.WriteLine("Time Elapsed : {0}",
+                stopwatch.Elapsed);
+            Console.ReadLine();
+
+        }
+    }
+}
